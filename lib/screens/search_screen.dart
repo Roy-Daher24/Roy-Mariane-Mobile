8b21167cb0bfe63b42a2799bf7b40d8b6e6ba5e9
Mariane Sleiman <Marianne.Sleiman@inmind.ai>
2024-05-12 19:33:22 +0300

    Remove unused imports

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -4,8 +4,6 @@ import 'package:roy_mariane_mobile/screens/profile_screen.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
 import 'package:multi_select_flutter/util/multi_select_list_type.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
